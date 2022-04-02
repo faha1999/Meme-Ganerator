79861840d9a1412a018d6e4ed8d803fddc3e0394
faha1999 <kafaha1999@gmail.com>
2022-04-02 20:16:44 +0600

    Final commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header';
+import Meme from './components/Meme';
+
+import './css/style.min.css';
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Meme />
+    </div>
+  );
+}
